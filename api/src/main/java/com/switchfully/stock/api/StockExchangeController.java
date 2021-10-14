dd019f5064eb2833ca2ae137fcbc2a20c2c395c7
Klaas Devits <klaas.devits@gmail.com>
2021-10-14 17:00:54 +0200

    new compile error: poms of modules are supposedly missing

--- api/src/main/java/com/switchfully/stock/api/StockExchangeController.java
+++ api/src/main/java/com/switchfully/stock/api/StockExchangeController.java
@@ -2,7 +2,6 @@ package com.switchfully.stock.api;
 
 import com.switchfully.stock.service.StockService;
 
-
 public class StockExchangeController {
     private StockService service = new StockService();
 
